7f9e38389fae5e00a7929b03cb839689c80e3946
akhil-2kb2 <rudraaraju@gmail.com>
2025-03-13 10:28:06 +0530

    add first prog

---
+++ DataStructure/Arrays/1.ElementSum.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class ElementSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        greetUser();
+
+        int[] numbers = initialize(input);  // Initialize array with user input
+        int sum = sumElements(numbers);  // Compute sum
+
+        System.out.println("Total Sum Of Elements: " + sum);
+        System.out.println(".....Happy 😀 Coding.....");
+        System.out.println("......Enjoy Learning......");
+
+        input.close();
+    }
+
+    public static int[] initialize(Scanner input) {
+        int[] myInt = new int[10];
+        System.out.print("Enter 10 Integer Numbers:");
+        for (int i = 0; i < myInt.length; i++) {
+            myInt[i] = input.nextInt();  // Store user input in array
+        }
+        return myInt;
+    }
+
+    public static int sumElements(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;  // Add each element to sum
+        }
+        return sum;
+    }
+
+    public static void greetUser() {
+        System.out.println(">>>>>> Welcome To The Array Sum Zone <<<<<<");
+    }
+}
