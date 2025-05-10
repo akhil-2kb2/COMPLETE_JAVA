54b3c0db5333fec055d98572b6024d47c96fcd06
akhil-2kb2 <rudraaraju@gmail.com>
2025-05-10 10:17:33 +0530

    Kind of no checker

--- OOPs/modul01/src/random/Armer.java
+++ OOPs/modul01/src/random/Armer.java
@@ -1,5 +1,28 @@
 package random;
 
 public class Armer {
+    // Function to check if a number is an Armstrong number
+    public static boolean isArmstrong(int number) {
+        int original = number;
+        int sum = 0;
+        int digits = String.valueOf(number).length();
 
+        while (number != 0) {
+            int digit = number % 10;
+            sum += (int) Math.pow(digit, digits);
+            number /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Armstrong Numbers between 0 and 999999999 are:");
+
+        for (int i = 0; i <= 999999999; i++) {
+            if (isArmstrong(i)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
