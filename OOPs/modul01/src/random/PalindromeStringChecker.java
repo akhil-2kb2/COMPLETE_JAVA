4a3f836a14c7f34ad2a09ec909759e11fe8410e0
akhil-2kb2 <rudraaraju@gmail.com>
2025-05-21 17:53:22 +0530

    adding PalindromeStringChecker

---
+++ OOPs/modul01/src/random/PalindromeStringChecker.java
@@ -0,0 +1,36 @@
+package random;
+
+import java.util.Scanner;
+
+public class PalindromeStringChecker {
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to check if it's a Palindrome: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a Palindrome string.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome string.");
+        }
+
+        scanner.close();
+    }
+}
